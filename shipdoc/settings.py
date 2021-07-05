83af73c612d7992b076077c7643341800ae83cb4
Fernando Cruz <quattrococodrilo@gmail.com>
2021-07-05 04:11:42 -0500

    avances

--- shipdoc/settings.py
+++ shipdoc/settings.py
@@ -74,3 +74,65 @@ INDEX_COLUMN_ORDER = {
     'document': 5,
     'url': 6,
 }
+
+# Scholarship request responses.
+# ------------------------------------------------------------
+SPREADSHEET_SOLICITUD_BECA = '1RM-N99V2gs6hmNXebi-7lt2CR2kNNP4aaUgNDw0xgWw'
+
+SOLICITUD_BECA_RANGE = 'alumnos_datos'
+
+SOLICITUD_BECA_COLUMN_ORDER = {
+    'email': 0,
+    'date': 1,
+    'enrollment': 2,
+    'name': 3,
+    'last_name': 4,
+}
+
+# Scholarship request responses.
+# ------------------------------------------------------------
+SPREADSHEET_BOLETAS_INDEX = '1IrqXZvkf0yMGUmRKG9xvcWP3NNU2so56dpBzOQZnaQ4'
+
+BOLETAS_INDEX_RANGE = 'Boletas'
+
+BOLETAS_INDEX_COLUMN_ORDER = {
+    'enrollment': 0,
+    'url': 1,
+}
+
+# Scholarship Revisión y resultados de la convocatoria de becas
+# ------------------------------------------------------------
+SPREADSHEET_BECAS_REPORT_SEP = '1Crr63rRVYFElx9a3K8Tk7edDizzFN8YlEKuNl1o-irA'
+
+BECAS_REPORT_SEP_RANGE = 'request'
+
+BECAS_REPORT_SEP_COLUMN_ORDER = {
+    'enrollment': 0,
+    'name': 1,
+    'career': 2,
+    'percentage_old': 6,
+    'group': 7,
+    'percentage_new': 8,
+    'type': 9,
+    'scholarship_type': 10,
+    'average': 10,
+}
+
+# Scholarship request responses.
+# ------------------------------------------------------------
+SPREADSHEET_BOLETAS_INDEX = '1ytym5wvW4TYJ90DgEOuV07_eoAHDadj_oe8F5iXEJPI'
+
+BOLETAS_INDEX_RANGE = 'Becas_asignadas'
+
+BOLETAS_INDEX_COLUMN_ORDER = {
+    'email': 1,
+    'enrollment': 2,
+    'name': 3,
+    'career': 4,
+    'group': 5,
+    'average': 6,
+    'scholarship_type': 7,
+    'percentage_new': 10,
+    'type': 12,
+    'is_approved': 14,
+}
